e37000a62ca352d03a190c321bfa509c56b2b390
Ali Nurdin <alinurdin.dev@gmail.com>
2024-04-04 22:09:28 +0700

    fix contact page

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -2,6 +2,8 @@ import Header from "@/components/Header";
 import SectionTitle from "@/components/SectionTitle";
 import { Button, Input, Textarea } from "@nextui-org/react";
 import Link from "next/link";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub, faInstagram, faLinkedin } from "@fortawesome/free-brands-svg-icons";
 
 export default function Page() {
   return (
@@ -16,13 +18,22 @@ export default function Page() {
                 <p className="mt-10">Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis tempore molestias quisquam magnam illo repellat assumenda repudiandae labore impedit amet. Perspiciatis quia totam incidunt voluptatum fuga vitae, nostrum eum, ipsa laudantium necessitatibus repellat quo voluptatibus, ipsam nemo nulla tempore officiis aliquid aperiam aliquam cumque. Dolore, voluptatibus culpa. Delectus nobis neque exercitationem recusandae. Illum libero quos similique doloremque sit itaque. Quis officiis, libero dolor iusto odit illum nam cumque adipisci quia eos a doloremque? Quibusdam, repudiandae nihil velit ratione eveniet in, natus dolores id et, est eius corporis odit vero at! Corrupti consequatur nihil tenetur ad expedita ea nesciunt voluptates odio!</p>
                 <ul className="mt-10 flex justify-between">
                   <li>
-                    <Link href="https://www.instagram.com/iamalinurdin.porto">Instagram</Link>
+                    <Link href="https://www.instagram.com/iamalinurdin.porto" className="flex gap-2">
+                      <FontAwesomeIcon icon={faInstagram} width={24} height={24} />
+                      Instagram
+                    </Link>
                   </li>
                   <li>
-                    <Link href="https://www.linkedin.com/in/iamalinurdin">Linkedin</Link>
+                    <Link href="https://www.linkedin.com/in/iamalinurdin" className="flex gap-2">
+                      <FontAwesomeIcon icon={faLinkedin} width={24} height={24} />
+                      Linkedin
+                    </Link>
                   </li>
                   <li>
-                    <Link href="https://www.github.com/iamalinurdin">Github</Link>
+                    <Link href="https://www.github.com/iamalinurdin" className="flex gap-2">
+                      <FontAwesomeIcon icon={faGithub} width={24} height={24} />
+                      Github
+                    </Link>
                   </li>
                 </ul>
               </div>
